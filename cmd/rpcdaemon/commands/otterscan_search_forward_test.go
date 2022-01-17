5af66db5021a011e58e6e03d48727bb19aefd72c
Willian Mitsuda <wmitsuda@gmail.com>
2022-01-17 14:11:52 -0300

    Extract common test utils

--- cmd/rpcdaemon/commands/otterscan_search_forward_test.go
+++ cmd/rpcdaemon/commands/otterscan_search_forward_test.go
@@ -39,30 +39,6 @@ func newMockForwardChunkProvider(chunks [][]byte) ChunkProvider {
 	}
 }
 
-func createBitmap(t *testing.T, blocks []uint64) []byte {
-	bm := roaring64.NewBitmap()
-	bm.AddMany(blocks)
-
-	chunk, err := bm.ToBytes()
-	if err != nil {
-		t.Fatal(err)
-	}
-	return chunk
-}
-
-func checkNext(t *testing.T, blockProvider BlockProvider, expectedBlock uint64, expectedHasNext bool) {
-	bl, hasNext, err := blockProvider()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if bl != expectedBlock {
-		t.Fatalf("Expected block %d, received %d", expectedBlock, bl)
-	}
-	if expectedHasNext != hasNext {
-		t.Fatalf("Expected hasNext=%t, received=%t; at block=%d", expectedHasNext, hasNext, expectedBlock)
-	}
-}
-
 func TestForwardBlockProviderWith1Chunk(t *testing.T) {
 	// Mocks 1 chunk
 	chunk1 := createBitmap(t, []uint64{1000, 1005, 1010})
