98b6603b0a4fe9491c414488dbb4a727cfe7d2f3
Willian Mitsuda <wmitsuda@gmail.com>
2022-02-18 02:05:44 -0300

    Extract ots_getBlockDetails into its own file

--- cmd/rpcdaemon/commands/otterscan_api.go
+++ cmd/rpcdaemon/commands/otterscan_api.go
@@ -399,46 +399,6 @@ func (api *OtterscanAPIImpl) getBlockWithSenders(number rpc.BlockNumber, tx kv.T
 	return block, senders, err
 }
 
-func (api *OtterscanAPIImpl) GetBlockDetails(ctx context.Context, number rpc.BlockNumber) (map[string]interface{}, error) {
-	tx, err := api.db.BeginRo(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer tx.Rollback()
-
-	b, senders, err := api.getBlockWithSenders(number, tx)
-	if err != nil {
-		return nil, err
-	}
-	if b == nil {
-		return nil, nil
-	}
-
-	chainConfig, err := api.chainConfig(tx)
-	if err != nil {
-		return nil, err
-	}
-
-	getBlockRes, err := api.delegateGetBlockByNumber(tx, b, number, false)
-	if err != nil {
-		return nil, err
-	}
-	getIssuanceRes, err := api.delegateIssuance(tx, b, chainConfig)
-	if err != nil {
-		return nil, err
-	}
-	feesRes, err := api.delegateBlockFees(ctx, tx, b, senders, chainConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	response := map[string]interface{}{}
-	response["block"] = getBlockRes
-	response["issuance"] = getIssuanceRes
-	response["totalFees"] = hexutil.Uint64(feesRes)
-	return response, nil
-}
-
 func (api *OtterscanAPIImpl) GetBlockTransactions(ctx context.Context, number rpc.BlockNumber, pageNumber uint8, pageSize uint8) (map[string]interface{}, error) {
 	tx, err := api.db.BeginRo(ctx)
 	if err != nil {
