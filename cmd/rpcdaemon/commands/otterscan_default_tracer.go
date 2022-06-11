ab29fc3adb6f147782f6e0c47b154b5b5f99aa02
Willian Mitsuda <wmitsuda@gmail.com>
2022-06-11 17:05:34 -0300

    Add docs

--- cmd/rpcdaemon/commands/otterscan_default_tracer.go
+++ cmd/rpcdaemon/commands/otterscan_default_tracer.go
@@ -8,6 +8,9 @@ import (
 	"github.com/ledgerwatch/erigon/core/vm"
 )
 
+// Helper implementation of vm.Tracer; since the interface is big and most
+// custom tracers implement just a few of the methods, this is a base struct
+// to avoid lots of empty boilerplate code
 type DefaultTracer struct {
 }
 
